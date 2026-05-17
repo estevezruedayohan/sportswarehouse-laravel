cdf8b713057a684eed0dd9957351415d34673d61
Yohan Estevez <estevezruedayohan@gmail.com>
2026-05-17 18:11:46 +1000

    Add index page components: implement search functionality, product gallery, and brand section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,80 @@
 @section('title', 'Home')
 @section('content')
 
-  <h1>Home Page</h1>
+  <search>
+    <form class="search-wrapper" action="#">
+      <label for="search-input" aria-label="Find a Product"></label>
+      <input type="search" class="search-input" id="search-input" placeholder="Search products"
+        aria-label="Enter the name of the product" />
+      <button type="submit" class="search-button" aria-label="Submit search">
+        <i class="fa fa-search" aria-hidden="true"></i>
+      </button>
+    </form>
+  </search>
+  <div class="cat-navigation">
+    <ul class="cat-links" aria-label="Product Categories">
+      <li class="cat-navigation-item">
+        <a href="#">Shoes</a>
+      </li>
+      <li class="cat-navigation-item">
+        <a href="#">Helmets</a>
+      </li>
+      <li class="cat-navigation-item">
+        <a href="#">Pants</a>
+      </li>
+      <li class="cat-navigation-item">
+        <a href="#">Tops</a>
+      </li>
+      <li class="cat-navigation-item">
+        <a href="#">Balls</a>
+      </li>
+      <li class="cat-navigation-item">
+        <a href="#">Equipment</a>
+      </li>
+      <li class="cat-navigation-item">
+        <a href="#">Training gear</a>
+      </li>
+    </ul>
+  </div>
 
-  <p>
-    <strong>Page in construction !!</strong>
-  </p>
+  <div class="main-tittle-container">
+    <h2 aria-label="Products list" class="main-tittle">
+      Featured Products
+    </h2>
+  </div>
+  @include('partials._indexGallery')
+  <section class="brands-section">
+    <div class="section-tittle-container">
+      <h2 aria-label="Our brands and Partnerships" class="section-tittle">
+        Our brands and partnerships
+      </h2>
+    </div>
+    <div class="brands-section__content">
+      <div class="section-content">
+        <p>These are some of our top brands and partnerships</p>
+        <p>The best of the best is here</p>
+      </div>
+      <div class="container-brand-image">
+        <div class="brand-image">
+          <img src="{{ asset('images/brands/logo_nike_1.png') }}" alt="Logo Nike" />
+        </div>
+        <div class="brand-image">
+          <img src="{{ asset('images/brands/logo_adidas_1.png') }}" alt="Logo Adidas" />
+        </div>
+        <div class="brand-image">
+          <img src="{{ asset('images/brands/logo_skins_1.png') }}" alt="Logo Skins" />
+        </div>
+        <div class="brand-image">
+          <img src="{{ asset('images/brands/logo_asics_1.png') }}" alt="Logo Asics" />
+        </div>
+        <div class="brand-image">
+          <img src="{{ asset('images/brands/logo_newbalance_1.png') }}" alt="Logo Newbalance" />
+        </div>
+        <div class="brand-image">
+          <img src="{{ asset('images/brands/logo_wilson_1.png') }}" alt="Logo Wilson" />
+        </div>
+      </div>
+    </div>
+  </section>
 
 @endsection
---
+++ resources/views/partials/_indexGallery.blade.php
@@ -0,0 +1,92 @@
+  <div class="gallery">
+    <article class="gallery__item gallery-card">
+      <div class="gallery-card__photo-frame full-centred">
+        <img src="./images/products/prod-image.jpg" alt="Adidas Euro 16 Top Soccer Ball" class="gallery-card__photo" />
+      </div>
+      <div class="gallery-card__prices full-centred">
+        <div class="offer-prices full-centred">
+          <span class="gallery-card__discounted-price"> $34.95 </span>
+          <span class="gallery-card__previous-price">
+            WAS <span>$46.00</span>
+          </span>
+        </div>
+
+        <span class="gallery-card__normal-price visually-hidden">$70.00</span>
+      </div>
+      <h3 class="gallery-card__product-name">
+        adidas Euro16 Top Soccer Ball
+      </h3>
+    </article>
+    <article class="gallery__item gallery-card">
+      <div class="gallery-card__photo-frame full-centred">
+        <img src="./images/products/helmet.jpg" alt="Pro-tec Classic Skate Helmet" class="gallery-card__photo" />
+      </div>
+      <div class="gallery-card__prices full-centred">
+        <div class="offer-prices full-centred visually-hidden">
+          <span class="gallery-card__discounted-price"> $34.95 </span>
+          <span class="gallery-card__previous-price">
+            WAS <span>$46.00</span>
+          </span>
+        </div>
+
+        <span class="gallery-card__normal-price">$70.00</span>
+      </div>
+      <h3 class="gallery-card__product-name">
+        Pro-tec Classic Skate Helmet
+      </h3>
+    </article>
+    <article class="gallery__item gallery-card">
+      <div class="gallery-card__photo-frame full-centred">
+        <img src="./images/products/waterBottle.jpg" alt="Nike Sport 600ml Water Bottle" class="gallery-card__photo" />
+      </div>
+      <div class="gallery-card__prices full-centred">
+        <div class="offer-prices full-centred">
+          <span class="gallery-card__discounted-price"> $15.00 </span>
+          <span class="gallery-card__previous-price">
+            WAS <span>$17.50</span>
+          </span>
+        </div>
+
+        <span class="gallery-card__normal-price visually-hidden">$70.00</span>
+      </div>
+      <h3 class="gallery-card__product-name">
+        Nike Sport 600ml Water Bottle
+      </h3>
+    </article>
+    <article class="gallery__item gallery-card">
+      <div class="gallery-card__photo-frame full-centred">
+        <img src="./images/products/boxingGloves.jpg" alt="Boxing Gloves Sting ArmaPlus" class="gallery-card__photo" />
+      </div>
+      <div class="gallery-card__prices full-centred">
+        <div class="offer-prices full-centred visually-hidden">
+          <span class="gallery-card__discounted-price"> $34.95 </span>
+          <span class="gallery-card__previous-price">
+            WAS <span>$46.00</span>
+          </span>
+        </div>
+
+        <span class="gallery-card__normal-price">$79.95</span>
+      </div>
+      <h3 class="gallery-card__product-name">
+        Sting ArmaPlus Boxing Gloves
+      </h3>
+    </article>
+    <article class="gallery__item gallery-card">
+      <div class="gallery-card__photo-frame full-centred">
+        <img src="./images/products/footyBoots.jpg" alt="ASICS Football boot" class="gallery-card__photo" />
+      </div>
+      <div class="gallery-card__prices full-centred">
+        <div class="offer-prices full-centred">
+          <span class="gallery-card__discounted-price"> $15.00 </span>
+          <span class="gallery-card__previous-price">
+            WAS <span>$17.50</span>
+          </span>
+        </div>
+
+        <span class="gallery-card__normal-price visually-hidden">$70.00</span>
+      </div>
+      <h3 class="gallery-card__product-name">
+        Asics Gel Lethal Tigreor 8 IT Mens's
+      </h3>
+    </article>
+  </div>
