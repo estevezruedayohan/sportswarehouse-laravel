b94a4417a91168e77bc509d63ec32ebb4e4e605b
Yohan Estevez <estevezruedayohan@gmail.com>
2026-05-24 22:33:41 +1000

    Add product images for skate helmet and soccer ball; update index view to use object properties from CategoryController

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,31 +29,28 @@
   </div>
 
   <div class="gallery">
-    @foreach (config('common.products') as $product)
+    @foreach ($featuredProducts as $product)
       <article class="gallery__item gallery-card">
         <div class="gallery-card__photo-frame full-centred">
-          <img src="{{ asset('images/products/' . $product['image']) }}" alt="{{ $product['alt'] }}"
+          <img src="{{ asset('images/products/' . $product->image) }}" alt="{{ $product->alt }}"
             class="gallery-card__photo" />
         </div>
         <div class="gallery-card__prices full-centred">
-          @if ($product['discount'] > 0)
-            @php
-              $discounted_price = $product['normal_price'] * (1 - $product['discount'] / 100);
-            @endphp
+          @if ($product->saleprice)
             <div class="offer-prices full-centred">
               <span class="gallery-card__discounted-price">
-                ${{ number_format($discounted_price, 2, '.', ',') }} </span>
+                ${{ number_format($product->saleprice, 2, '.', ',') }} </span>
               <span class="gallery-card__previous-price">
-                WAS <span>${{ number_format($product['normal_price'], 2, '.', '.') }}</span>
+                WAS <span>${{ number_format($product->price, 2, '.', '.') }}</span>
               </span>
             </div>
           @else
-            <span class="gallery-card__normal-price">${{ number_format($product['normal_price'], 2, '.', ',') }}</span>
+            <span class="gallery-card__normal-price">${{ number_format($product->price, 2, '.', ',') }}</span>
           @endif
 
         </div>
         <h3 class="gallery-card__product-name">
-          {{ $product['product_name'] }}
+          {{ $product->name }}
         </h3>
       </article>
     @endforeach
