864d1a611018fe82411bf25f5ead8e5a3c60fce6
Yohan Estevez <estevezruedayohan@gmail.com>
2026-05-24 19:29:32 +1000

    Add migrations for categories and products tables

---
+++ database/migrations/2026_05_24_085533_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 250);
+            $table->string('image', 250)->nullable();
+            $table->decimal('price', 10, 2);
+            $table->decimal('saleprice', 10, 2)->nullable();
+            $table->string('description', 2000);
+            $table->tinyInteger('featured')->default(0);
+            $table->unsignedBigInteger('categoryId');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
