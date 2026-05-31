aa57497563a3cf658dc4ddbd44e134f287bfb878
Yohan Estevez <estevezruedayohan@gmail.com>
2026-05-31 20:06:42 +1000

    Refactor product filter into a separate partial view and update index and viewProducts templates to include it

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,25 +2,7 @@
 @section('title', 'Home')
 @section('content')
 
-  <search>
-    <form class="search-wrapper" action="#">
-      <label for="search-input" aria-label="Find a Product"></label>
-      <input type="search" class="search-input" id="search-input" placeholder="Search products"
-        aria-label="Enter the name of the product" />
-      <button type="submit" class="search-button" aria-label="Submit search">
-        <i class="fa fa-search" aria-hidden="true"></i>
-      </button>
-    </form>
-  </search>
-  <div class="cat-navigation">
-    <ul class="cat-links" aria-label="Product Categories">
-      @foreach (config('common.productsCategories') as $category)
-        <li class="cat-navigation-item">
-          <a href="{{ $category['url'] }}">{{ $category['name'] }}</a>
-        </li>
-      @endforeach
-    </ul>
-  </div>
+  @include('partials/_productsFilter')
 
   <section class="hero-banner">
     <div class="hero-banner-container">
---
+++ resources/views/partials/_productsFilter.blade.php
@@ -0,0 +1,19 @@
+<search>
+  <form class="search-wrapper" action="#">
+    <label for="search-input" aria-label="Find a Product"></label>
+    <input type="search" class="search-input" id="search-input" placeholder="Search products"
+      aria-label="Enter the name of the product" />
+    <button type="submit" class="search-button" aria-label="Submit search">
+      <i class="fa fa-search" aria-hidden="true"></i>
+    </button>
+  </form>
+</search>
+<div class="cat-navigation">
+  <ul class="cat-links" aria-label="Product Categories">
+    @foreach (config('common.productsCategories') as $category)
+      <li class="cat-navigation-item">
+        <a href="{{ $category['url'] }}">{{ $category['name'] }}</a>
+      </li>
+    @endforeach
+  </ul>
+</div>
