2fcec89024a46a6c484da43959b55a2fb56a0419
Yohan Estevez <estevezruedayohan@gmail.com>
2026-06-02 19:33:07 +1000

    Refactor CSS for error page and add btn-secondary styles; clean up search input markup

--- resources/views/partials/_productsSearch.blade.php
+++ resources/views/partials/_productsSearch.blade.php
@@ -1,8 +1,8 @@
 <search>
   <form class="search-wrapper" action="{{ route('product.search') }}" method="GET">
     {{-- <label for="search-input" aria-label="Find a Product"></label> --}}
-    <input type="search" class="search-input" name="query" {{-- id="search-input"  --}} value="{{ request('query') }}"
-      placeholder="Search products" aria-label="Enter the name of the product" />
+    <input type="search" class="search-input" name="query" value="{{ request('query') }}" placeholder="Search products"
+      aria-label="Enter the name of the product" />
     <button type="submit" class="search-button" aria-label="Submit search">
       <i class="fa fa-search" aria-hidden="true"></i>
     </button>
